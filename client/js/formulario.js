f11a183055b20df3a8d0a792518d6cef51bd5ab1
LeeonardoVargas <leu1607@hotmail.com>
2018-08-06 09:58:38 -0300

    Questionario com duas questões funcionando

--- client/js/formulario.js
+++ client/js/formulario.js
@@ -1,10 +1,12 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-var contador = 0;
+var contadorQ1;
+var contadorQ2;
 
 Template.formulario.onRendered(function() {
-    contador = 0;
+    contadorQ1 = 0;
+    contadorQ2 = 0;
 });
 
 Template.formulario.helpers({
@@ -12,31 +14,38 @@ Template.formulario.helpers({
 });
 
 Template.formulario.events({
-    'click #novo-item': function(event){
-        Bert.alert("Novo item", "info", "growl-bottom-left");
-
-        let mDiv = document.getElementById('itens-servico');
-        const block_to_insert = document.createElement('div');
-        block_to_insert.innerHTML = blocoForm();
-        mDiv.appendChild( block_to_insert );
-
-        contador = contador + 1;
+    'click #nova-resposta-q1': function(event){
+        novoBloco(1);
+    },
+    'click #nova-resposta-q2': function(event){
+        novoBloco(2);
     },
     'submit .formulario-uffs': function(){
-        // let aux = 'descItem' + 1;
-        // let teste = event.target.testeForm.value;
-        // let teste2 = event.target[aux].value;
-        // alert("saida: " + saida);
-    },
-    'click #btn-teste': function(event){
-        let aux = "";
-        for (let i = 0; i < contador; i++) {
-            aux = aux + " " + i;
-        }
-        Bert.alert("teste:" + aux, "danger", "growl-top-left");
+        return false;
     }
 })
 
-let blocoForm = function(){
-    return('<div class="input-field col m12"><input name="descItem'+contador+'" type="text"><label for="descItem'+contador+'">Descrição</label></div>');
+var novoBloco = function(nQuestao){
+    let valorID_1 = "", valorID_2 = "", divResposta = "resposta-q" + nQuestao;
+    if(nQuestao == 1){
+        valorID_1 = "r1descricao"+ contadorQ1;
+        valorID_2 = "r1justificativa"+ contadorQ1;
+        contadorQ1 = contadorQ1 + 1;
+    } else if(nQuestao == 2){
+        valorID_1 = "r2descricao"+ contadorQ2;
+        valorID_2 = "r2justificativa"+ contadorQ2;
+        contadorQ2 = contadorQ2 + 1;
+    }
+
+    Bert.alert("Nova resposta na questão ["+nQuestao+"]", "info", "growl-top-left");
+
+    let mDiv = document.getElementById(divResposta);
+    const block_to_insert = document.createElement('div');
+    block_to_insert.innerHTML = blocoForm(valorID_1, valorID_2);
+    mDiv.appendChild( block_to_insert );
+
+}
+
+let blocoForm = function(valorID_1, valorID_2){
+    return('<div class="input-field col s12 m6"><textarea id="'+valorID_1+'" class="materialize-textarea"></textarea><label for="'+valorID_1+'">Descrição detalhada</label></div><div class="input-field col s12 m6"><textarea id="'+valorID_2+'" class="materialize-textarea"></textarea><label for="'+valorID_2+'">Justificativa da necessidade</label></div>');
 }
