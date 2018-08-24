6f518d4c90e0bdea2307f199732f114f26171915
LeeonardoVargas <leu1607@hotmail.com>
2018-08-24 11:25:45 -0300

    Alterando users

--- server/seeder.js
+++ server/seeder.js
@@ -2,20 +2,12 @@ Meteor.startup(function(){
     if (Meteor.users.find().count() === 0){
         // Super User
         Accounts.createUser({
-            username: 'super',
+            username: 'superUFFS',
             email: 'Super@user.com',
-            password: 'uva007',
+            password: 'uffspdtic007',
         });
 
         console.log("[1] Usuário SUPER Criado");
-
-        Accounts.createUser({
-            username: 'teste',
-            email: 'teste@teste.com',
-            password: 'teste',
-        });
-
-        console.log("[2] Usuário TESTE Criado");
 
     }
 })
