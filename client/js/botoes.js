76759987af68c900fd9b539892df6e2b1902ac45
LeeonardoVargas <leu1607@hotmail.com>
2018-09-12 10:53:35 -0300

    Arquivo slq estruturado

--- client/js/botoes.js
+++ client/js/botoes.js
@@ -1,70 +1,19 @@
 import { Template } from 'meteor/templating';
 
-var cabecarioSQL = "
-CREATE TABLE identificacao_pdtic( \
-    id_usuario uuid,\
-    nome varchar,\
-    localizacao varchar(60),\
-    perfil varchar(10)\
-);\
-
-CREATE TABLE questao1_pdtic(\
-    descricao varchar,\
-    justificativa varchar,\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao2_pdtic(\
-    descricao varchar,\
-    justificativa varchar,\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao3_pdtic(\
-    descricao varchar,\
-    justificativa varchar,\
-    sistema varchar(60),\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao4_pdtic(\
-    sistema varchar(60),\
-    classificacao varchar(14),\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao5_pdtic(\
-    servico varchar(60),\
-    classificacao varchar(14),\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao6_pdtic(\
-    equipamento varchar(60),\
-    classificacao varchar(14),\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao7_pdtic(\
-    descricao varchar,\
-    justificativa varchar,\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE questao8_pdtic(\
-    consideracao,\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);\
-CREATE TABLE sem_resposta(\
-    num_questao int,\
-    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES identificacao_pdtic(id_usuario),\
-);";
+var tuplas = [];
 
 Template.botoes.events({
     'click #gerar-sql': function(event){
         const result = Respostas.find({}, {sort:  {numero: 1}});
-         result.forEach((resposta) => {
-            console.log("\n\n\nUser: " + resposta.nome);
-            for(let k = 1; k < 9; k++){
-                const aux = 'questao'+k;
-                obterRespostas(resposta[aux], k);
-            }
+            result.forEach((resposta) => {
+                const usuarioID = resposta._id;
+                tuplas.push('INSERT INTO identificacao_pdtic VALUES ("' + usuarioID + '", "' + resposta.nome + '", "' + resposta.localizacao + '", "' + resposta.papel + '");');
+                for(let k = 1; k < 9; k++){
+                    const aux = 'questao'+k;
+                    obterRespostas(usuarioID, resposta[aux], k);
+                }
         });
-        let olar = "olar";
-        Router.go("/text/"+cabecarioSQL);
+        Router.go("/text/"+tuplas);
     },
     'click #encerrar-sesao': function(event){
         Meteor.logout(function(err){
@@ -78,27 +27,24 @@ Template.botoes.events({
     },
 });
 
-const obterRespostas = (respostas, nQuestao) =>{
-    if(nQuestao < 4 || nQuestao == 7){
-        if(respostas == 'Sem resposta'){
-            console.log("Sem resposta");
-        }else if(nQuestao == 3){
-            console.log("[if] questão 3");
-        } else {
-            console.log("[if] questão " + nQuestao);
-        }
-    } else if(nQuestao > 3 && nQuestao < 8){
-        for(r in respostas){
-            if('c' in respostas[r]){
-                console.log("sistema: " + respostas[r]['s'] + ", classificacao: " + respostas[r]['c']);
-            }
-        }
-        console.log("[else if] questão " + nQuestao);
+const obterRespostas = (usuarioID, respostas, nQuestao) =>{
+    if(respostas == 'Sem resposta'){
+        tuplas.push('INSERT INTO sem_resposta VALUES (' + nQuestao + ', "'+ usuarioID + '");');
     } else {
-        if(respostas == 'Sem resposta'){
-            console.log("Sem resposta");
-        } else {
-            console.log("[else] questão " + nQuestao);
+        for(r in respostas){
+            if(nQuestao < 4 || nQuestao == 7){ // QUESTÕES: 1,2,3,7
+                if(nQuestao == 3){
+                    tuplas.push('INSERT INTO questao3_pdtic VALUES ("' + respostas[r]['d'] + '", "' + respostas[r]['j'] + '", "' + respostas[r]['s'] + '", "'+ usuarioID + '");');
+                } else {
+                    tuplas.push('INSERT INTO questao' + nQuestao + '_pdtic VALUES ("' + respostas[r]['d'] + '", "' + respostas[r]['j'] + '", "'+ usuarioID + '");');
+                }
+            } else if(nQuestao > 3 && nQuestao < 7){ // QUESTÕES: 4,5,6
+                if('c' in respostas[r]){
+                    tuplas.push('INSERT INTO questao' + nQuestao + '_pdtic VALUES ("' + respostas[r]['s'] + '", "' + respostas[r]['c'] + '", "'+ usuarioID + '");');
+                }
+            } else { // QUESTÃO: 8
+                tuplas.push('INSERT INTO questao8_pdtic VALUES ("' + respostas[r]['n'] + '", "'+ usuarioID + '");');
+            }
         }
     }
 };
