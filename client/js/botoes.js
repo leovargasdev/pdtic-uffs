2f1bf55affe7141c6be42d3f0f91359adc899fac
LeeonardoVargas <leu1607@hotmail.com>
2018-09-12 19:50:39 -0300

    Versão final do gerador do sql

--- client/js/botoes.js
+++ client/js/botoes.js
@@ -7,7 +7,7 @@ Template.botoes.events({
         const result = Respostas.find({}, {sort:  {numero: 1}});
             result.forEach((resposta) => {
                 const usuarioID = resposta._id;
-                tuplas.push('INSERT INTO identificacao_pdtic VALUES ("' + usuarioID + '", "' + resposta.nome + '", "' + resposta.localizacao + '", "' + resposta.papel + '");');
+                tuplas.push("INSERT INTO identificacao_pdtic (id_usuario, nome, localizacao, perfil)  VALUES ('" + usuarioID + "', '" + resposta.nome + "', '" + resposta.localizacao + "', '" + resposta.papel + "');");
                 for(let k = 1; k < 9; k++){
                     const aux = 'questao'+k;
                     obterRespostas(usuarioID, resposta[aux], k);
@@ -29,21 +29,25 @@ Template.botoes.events({
 
 const obterRespostas = (usuarioID, respostas, nQuestao) =>{
     if(respostas == 'Sem resposta'){
-        tuplas.push('INSERT INTO sem_resposta VALUES (' + nQuestao + ', "'+ usuarioID + '");');
+        tuplas.push("INSERT INTO sem_resposta (num_questao, id_usuario) VALUES (" + nQuestao + ", '"+ usuarioID + "');");
     } else {
         for(r in respostas){
             if(nQuestao < 4 || nQuestao == 7){ // QUESTÕES: 1,2,3,7
                 if(nQuestao == 3){
-                    tuplas.push('INSERT INTO questao3_pdtic VALUES ("' + respostas[r]['d'] + '", "' + respostas[r]['j'] + '", "' + respostas[r]['s'] + '", "'+ usuarioID + '");');
+                    tuplas.push("INSERT INTO questao3_pdtic (descricao, justificativa, sistema, id_usuario) VALUES ('" + respostas[r]['d'] + "', '" + respostas[r]['j'] + "', '" + respostas[r]['s'] + "', '" + usuarioID + "');");
                 } else {
-                    tuplas.push('INSERT INTO questao' + nQuestao + '_pdtic VALUES ("' + respostas[r]['d'] + '", "' + respostas[r]['j'] + '", "'+ usuarioID + '");');
+                    tuplas.push("INSERT INTO questao" + nQuestao + "_pdtic (descricao, justificativa, id_usuario) VALUES ('" + respostas[r]['d'] + "', '" + respostas[r]['j'] + "', '"+ usuarioID + "');");
                 }
             } else if(nQuestao > 3 && nQuestao < 7){ // QUESTÕES: 4,5,6
                 if('c' in respostas[r]){
-                    tuplas.push('INSERT INTO questao' + nQuestao + '_pdtic VALUES ("' + respostas[r]['s'] + '", "' + respostas[r]['c'] + '", "'+ usuarioID + '");');
+                    let aux;
+                    if(nQuestao == 4) aux = "sistema";
+                    else if(nQuestao == 5) aux = "servico";
+                    else aux = "equipamento";
+                    tuplas.push("INSERT INTO questao" + nQuestao + "_pdtic (" + aux + ", classificacao, id_usuario) VALUES ('" + respostas[r]['s'].replace("/", "_") + "', '" + respostas[r]['c'] + "', '" + usuarioID + "');");
                 }
             } else { // QUESTÃO: 8
-                tuplas.push('INSERT INTO questao8_pdtic VALUES ("' + respostas[r]['n'] + '", "'+ usuarioID + '");');
+                tuplas.push("INSERT INTO questao8_pdtic (consideracao, id_usuario) VALUES ('" + respostas[r]['n'] + "', '" + usuarioID + "');");
             }
         }
     }
