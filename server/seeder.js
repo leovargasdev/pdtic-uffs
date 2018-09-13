ebfa038b74660f2032dd753a23bae3a7f1f5aba9
LeeonardoVargas <leu1607@hotmail.com>
2018-09-13 10:44:59 -0300

    Colocando rota para gerar o slq

--- server/seeder.js
+++ server/seeder.js
@@ -1,12 +1,6 @@
 Meteor.startup(function(){
     if (Meteor.users.find().count() === 0){
         // Super User
-        Accounts.createUser({
-            username: 'superUFFS',
-            email: 'Super@user.com',
-            password: 'uffspdtic007',
-        });
-
         console.log("[1] Usuário SUPER Criado");
 
     }
