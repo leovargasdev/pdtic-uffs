1eb0bb03242e3ee0720c5fa56d33e57de1965992
LeeonardoVargas <leu1607@hotmail.com>
2018-10-01 08:42:33 -0300

    Desativando formulario

--- client/js/botoes.js
+++ client/js/botoes.js
@@ -2,18 +2,46 @@ import { Template } from 'meteor/templating';
 
 var tuplas = [];
 
+tuplas.push({ valor: 'CREATE TABLE identificacao_pdtic( id_usuario varchar, nome varchar, localizacao varchar(60), perfil varchar(10), CONSTRAINT identificacao_pdtic_pkey PRIMARY KEY (id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao1( descricao varchar, justificativa varchar, id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao2( descricao varchar, justificativa varchar, id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao3( descricao varchar, justificativa varchar, sistema varchar(60), id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao4( sistema varchar(60), classificacao varchar(14), id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao5( servico varchar(60), classificacao varchar(14), id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao6( equipamento varchar(60), classificacao varchar(14), id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao7( descricao varchar, justificativa varchar, id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE questao8( consideracao varchar, id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+tuplas.push({ valor: 'CREATE TABLE sem_resposta( num_questao int, id_usuario varchar, CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES public.identificacao_pdtic(id_usuario));'});
+
+Template.sql.helpers({
+    'respostas': function(){
+        const result = Respostas.find({}, {sort:  {numero: 1}});
+        result.forEach((resposta) => {
+            const usuarioID = resposta._id;
+            tuplas.push({ valor: "INSERT INTO identificacao_pdtic (id_usuario, nome, localizacao, perfil)  VALUES ('" + usuarioID + "', '" + resposta.nome + "', '" + resposta.localizacao + "', '" + resposta.papel + "');"});
+            for(let k = 1; k < 9; k++){
+                const aux = 'questao'+k;
+                obterRespostas(usuarioID, resposta[aux], k);
+            }
+        });
+        return tuplas;
+    },
+})
+
 Template.botoes.events({
     'click #gerar-sql': function(event){
         const result = Respostas.find({}, {sort:  {numero: 1}});
             result.forEach((resposta) => {
                 const usuarioID = resposta._id;
-                tuplas.push("INSERT INTO identificacao_pdtic (id_usuario, nome, localizacao, perfil)  VALUES ('" + usuarioID + "', '" + resposta.nome + "', '" + resposta.localizacao + "', '" + resposta.papel + "');");
-                for(let k = 1; k < 9; k++){
-                    const aux = 'questao'+k;
-                    obterRespostas(usuarioID, resposta[aux], k);
-                }
+                console.log(usuarioID);
+                // tuplas.push("INSERT INTO identificacao_pdtic (id_usuario, nome, localizacao, perfil)  VALUES ('" + usuarioID + "', '" + resposta.nome + "', '" + resposta.localizacao + "', '" + resposta.papel + "');");
+                // for(let k = 1; k < 9; k++){
+                //     const aux = 'questao'+k;
+                //     obterRespostas(usuarioID, resposta[aux], k);
+                // }
         });
-        Router.go("/text/"+tuplas);
+        // console.log(tuplas);
+        // Router.go("/text/"+tuplas);
     },
     'click #encerrar-sesao': function(event){
         Meteor.logout(function(err){
@@ -29,14 +57,14 @@ Template.botoes.events({
 
 const obterRespostas = (usuarioID, respostas, nQuestao) =>{
     if(respostas == 'Sem resposta'){
-        tuplas.push("INSERT INTO sem_resposta (num_questao, id_usuario) VALUES (" + nQuestao + ", '"+ usuarioID + "');");
+        tuplas.push({ valor: "INSERT INTO sem_resposta (num_questao, id_usuario) VALUES (" + nQuestao + ", '"+ usuarioID + "');"});
     } else {
         for(r in respostas){
             if(nQuestao < 4 || nQuestao == 7){ // QUESTÕES: 1,2,3,7
                 if(nQuestao == 3){
-                    tuplas.push("INSERT INTO questao3 (descricao, justificativa, sistema, id_usuario) VALUES ('" + respostas[r]['d'] + "', '" + respostas[r]['j'] + "', '" + respostas[r]['s'] + "', '" + usuarioID + "');");
+                    tuplas.push({ valor: "INSERT INTO questao3 (descricao, justificativa, sistema, id_usuario) VALUES ('" + respostas[r]['d'].replace(/[\']/g, "").replace(/"/g, "") + "', '" + respostas[r]['j'].replace(/[\']/g, "").replace(/"/g, "") + "', '" + respostas[r]['s'] + "', '" + usuarioID + "');"});
                 } else {
-                    tuplas.push("INSERT INTO questao" + nQuestao + " (descricao, justificativa, id_usuario) VALUES ('" + respostas[r]['d'] + "', '" + respostas[r]['j'] + "', '"+ usuarioID + "');");
+                    tuplas.push({ valor: "INSERT INTO questao" + nQuestao + " (descricao, justificativa, id_usuario) VALUES ('" + respostas[r]['d'].replace(/[\']/g, "").replace(/"/g, "") + "', '" + respostas[r]['j'].replace(/[\']/g, "").replace(/"/g, "") + "', '"+ usuarioID + "');"});
                 }
             } else if(nQuestao > 3 && nQuestao < 7){ // QUESTÕES: 4,5,6
                 if('c' in respostas[r]){
@@ -44,10 +72,10 @@ const obterRespostas = (usuarioID, respostas, nQuestao) =>{
                     if(nQuestao == 4) aux = "sistema";
                     else if(nQuestao == 5) aux = "servico";
                     else aux = "equipamento";
-                    tuplas.push("INSERT INTO questao" + nQuestao + " (" + aux + ", classificacao, id_usuario) VALUES ('" + respostas[r]['s'].replace("/", "_") + "', '" + respostas[r]['c'] + "', '" + usuarioID + "');");
+                    tuplas.push({ valor: "INSERT INTO questao" + nQuestao + " (" + aux + ", classificacao, id_usuario) VALUES ('" + respostas[r]['s'] + "', '" + respostas[r]['c'] + "', '" + usuarioID + "');"});
                 }
             } else { // QUESTÃO: 8
-                tuplas.push("INSERT INTO questao8 (consideracao, id_usuario) VALUES ('" + respostas[r]['n'] + "', '" + usuarioID + "');");
+                tuplas.push({ valor: "INSERT INTO questao8 (consideracao, id_usuario) VALUES ('" + respostas[r]['n'].replace(/[\']/g, "").replace(/"/g, "") + "', '" + usuarioID + "');"});
             }
         }
     }
